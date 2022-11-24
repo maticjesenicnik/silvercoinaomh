5ee80c496e59b5b7c9d14d09578172aa62b462af
Alen Budimir <7889389+budi1200@users.noreply.github.com>
2022-11-24 17:29:52 +0100

    Homepage cover layout, socials json

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 
 export const Footer = () => {
+  const socialsData = require('/json/socials.json')
+
   return (
     <footer
       className={'bg-background text-on-background flex justify-between flex-col md:flex-row gap-4 items-center p-5'}
@@ -16,36 +18,17 @@ export const Footer = () => {
 
       <div className={'flex justify-center flex-col items-center'}>
         <div className={'flex gap-1 flex-wrap'}>
-          <a
-            className={'flex justify-center items-center hover:opacity-80 transition-all'}
-            href={'https://www.facebook.com/groups/399997464231407'}
-          >
-            <Image width={60} height={60} src={'/images/socials/fb.png'} alt={'Facebook'} />
-          </a>
-          <a
-            className={'flex justify-center items-center hover:opacity-80 transition-all'}
-            href={'https://www.instagram.com/silvercoinaomh/'}
-          >
-            <Image width={60} height={60} src={'/images/socials/ig.png'} alt={'Instagram'} />
-          </a>
-          <a
-            className={'flex justify-center items-center hover:opacity-80 transition-all'}
-            href={'https://boardgamegeek.com/boardgame/312682/silver-coin-age-monster-hunters'}
-          >
-            <Image width={100} height={60} src={'/images/socials/bgg.png'} alt={'BGG'} />
-          </a>
-          <a
-            className={'flex justify-center items-center hover:opacity-80 transition-all'}
-            href={'https://discord.gg/NfQqrSgW3u'}
-          >
-            <Image width={60} height={60} src={'/images/socials/discord.png'} alt={'Discord'} />
-          </a>
-          <a
-            className={'flex justify-center items-center hover:opacity-80 transition-all'}
-            href={'https://www.youtube.com/channel/UCJubiQxl7DSBweyIGmaK6-A'}
-          >
-            <Image width={60} height={60} src={'/images/socials/yt.png'} alt={'YouTube'} />
-          </a>
+          {socialsData.socials.map((el: any, index: number) => {
+            return (
+              <a
+                key={index}
+                className={'flex justify-center items-center hover:opacity-80 transition-all'}
+                href={el.url}
+              >
+                <Image width={60} height={60} src={el.image} alt={el.name} />
+              </a>
+            )
+          })}
         </div>
         <a href={'mailto:silvercoinaomh@gmail.com'}>silvercoinaomh@gmail.com</a>
       </div>
