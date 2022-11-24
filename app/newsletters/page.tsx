cd6bb6826c6dd652d120617b8acb36dc89e73813
maticjesenicnik <matic.jesenicnik@gmail.com>
2022-11-24 21:14:33 +0100

    link open new tab, image component, change info

--- app/newsletters/page.tsx
+++ app/newsletters/page.tsx
@@ -3,7 +3,7 @@ import rss from '../../public/json/newsletters.json';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const Newsletters = () => {
+const Newsletters = () : JSX.Element => {
   return (
     <div className='text-center'>
       <h1 className='text-4xl font-bold'>Newsletters</h1>
@@ -11,13 +11,15 @@ const Newsletters = () => {
       <div className="container mx-auto">
         <div className="grid grid-cols-1 gap-7 md:grid-cols-2 lg:grid-cols-3 mt-12 ml-12 mr-12">
           {rss.newsletters.map((newsletter) => (
-            <Link href={newsletter.url}>
+            <Link href={newsletter.url} target="_blank">
               <div className="bg-white/[.12] rounded-lg shadow-lg p-4 aspect-square pl-0 pr-0 pt-0 motion-safe:animate-fadeIn">
-                <img className="w-full h-full object-cover rounded-t-lg" src={"/" + newsletter.image} alt={newsletter.title} />
-                <div className="flex flex-col items-start m-1">
-                  <small className="text-gray-200 text-left">NEWSLETTER</small>
-                  <h2 className="text-xl font-bold text-center">{newsletter.title}</h2>
-                  <p className="text-gray-200 text-left">{newsletter.date}</p>
+                <Image className="w-full h-full object-cover rounded-t-lg" src={"/" + newsletter.image} alt={newsletter.title} width={400} height={400}/>
+                <div className="flex flex-col">
+                  <div className="flex flex-col pl-3 pr-3 pt-2">
+                    <p className="text-gray-200 text-start">NEWSLETTER</p>
+                    <p className="text-xl font-bold text-center">{newsletter.title}</p>
+                    <p className="text-gray-200 text-right">{newsletter.date}</p>
+                  </div>
                 </div>
               </div>
             </Link>
