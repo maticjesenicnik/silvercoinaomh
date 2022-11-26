ac555ed5a7dc1a4bc7cb862b6acab4a988b3be31
Alen Budimir <7889389+budi1200@users.noreply.github.com>
2022-11-26 13:55:17 +0100

    Add dropdown option to nav link

--- app/Header.tsx
+++ app/Header.tsx
@@ -17,16 +17,18 @@ export const Header = () => {
 
         <nav
           data-open={isOpen}
-          className="flex h-0 w-full grow flex-col items-center gap-4 overflow-hidden transition-all data-open:h-52 data-open:pt-4 md:h-auto md:w-auto md:grow-0 md:flex-row"
+          className="flex h-0 w-full grow flex-col items-center gap-4 transition-all data-open:h-52 data-open:pt-4 md:h-auto md:w-auto md:grow-0 md:flex-row"
         >
           <Navlink icon={'home'} text={'Home'} href={'/'} />
           <Navlink icon={'library_books'} text={'Learn'} href={'/'} />
           <Navlink icon={'videogame_asset'} text={'Playtest'} href={'/'} />
           <Navlink icon={'image'} text={'Gallery'} href={'/'} />
           <Navlink icon={'rss_feed'} text={'Newsletters'} href={'/newsletters/'} />
-          <Navlink icon={'explore'} text={'Stories'} href={'/lore/stories/'} />
-          <Navlink icon={'explore'} text={'Characters'} href={'/lore/characters/'} />
-          <Navlink icon={'explore'} text={'Kingdoms'} href={'/lore/kingdoms/'} />
+          <Navlink icon={'explore'} text={'Lore'} href={'/lore'}>
+            <Navlink icon={'explore'} text={'Stories'} href={'/lore/stories/'} />
+            <Navlink icon={'explore'} text={'Characters'} href={'/lore/characters/'} />
+            <Navlink icon={'explore'} text={'Kingdoms'} href={'/lore/kingdoms/'} />
+          </Navlink>
           <Navlink icon={'smart_display'} text={'Videos & Podcasts'} href={'/videos/'} />
           <Navlink icon={'groups'} text={'Team'} href={'/'} />
         </nav>
--- app/Navlink.tsx
+++ app/Navlink.tsx
@@ -1,8 +1,16 @@
-export const Navlink = ({ icon, text, href }: { icon: string; text: string; href: string }) => {
+export const Navlink = ({ icon, text, href, children }: any) => {
   return (
-    <a href={href} className={'flex gap-1.5 items-center rounded-md hover:bg-white/10 p-1 transition-all'}>
-      <span className="material-icons-outlined text-sm">{icon}</span>
-      {text}
-    </a>
+    <div className={'group relative'}>
+      <a href={href} className={'flex gap-1.5 items-center rounded-md hover:bg-white/10 p-1 transition-all'}>
+        <span className="material-icons-outlined text-sm">{icon}</span>
+        {text}
+      </a>
+
+      {children && children.length > 0 ? (
+        <div className="hidden group-hover:flex flex-col gap-1 border border-white/5 absolute top-[100%] left-0 z-10 bg-background p-2 rounded-md shadow-md">
+          {children}
+        </div>
+      ) : null}
+    </div>
   )
 }
