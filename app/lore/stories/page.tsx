9e87fbfc75904bda6dbd5906fd5cecc7c815f9dd
maticjesenicnik <matic.jesenicnik@gmail.com>
2022-12-08 00:16:38 +0100

    add header component to stories

--- app/lore/stories/page.tsx
+++ app/lore/stories/page.tsx
@@ -1,16 +1,16 @@
+import { PageHeader } from '../../../components/PageHeader'
+
 const Stories = (): JSX.Element => {
   const stories = require('/json/stories.json')
 
   return (
     <div className="text-center">
-      <title>Stories</title>
-      <h1 className="text-4xl font-bold">Stories</h1>
-      <p className="text-xl">Coming soon!</p>
+      <PageHeader title={'Stories'} />
 
       <div className="container mx-auto w-1/2">
         {stories.stories.map((story: any, index: number) => (
           <div className="flex flex-col mt-6">
-            <div className="text-3xl text-center">{story.title}</div>
+            <div className="text-5xl text-center">{story.title}</div>
             {story.chapters.map((chapter: any, index: number) => (
               <div className="mt-3 text-justify">{chapter}</div>
             ))}
