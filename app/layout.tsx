a32c470ae8251bb926d643204cb6c049b6d0726b
maticjesenicnik <matic.jesenicnik@gmail.com>
2022-12-08 00:14:55 +0100

    increase font size for better experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <body className={'bg-background'}>
         <Header />
-        <main className={'min-h-[82.95vh] pb-28 bg-[url("/images/bg_texture.webp")] text-white pt-20'}>{children}</main>
+        <main className={'min-h-[82.95vh] pb-28 bg-[url("/images/bg_texture.webp")] text-white pt-20 text-2xl'}>{children}</main>
         <Footer />
       </body>
     </html>
