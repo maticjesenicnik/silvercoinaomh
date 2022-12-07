76e6e6ccd37c0f3f7a44d3b476b813bc0aeecd6d
maticjesenicnik <matic.jesenicnik@gmail.com>
2022-12-08 00:15:52 +0100

    add title to the header component

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -3,11 +3,14 @@ import bgImage from '../public/cover_art.webp'
 
 export const PageHeader = ({ title }: any) => {
   return (
-    <div className={'relative mb-20'}>
-      <Image className={'absolute w-full h-full object-cover'} src={bgImage} alt={'Background'} />
-      <div className={'relative w-full h-full bg-black/40 py-32'}>
-        <h1 className={'text-5xl font-bold text-center'}>{title}</h1>
+    <>
+      <title>{title}</title>
+      <div className={'relative mb-20'}>
+        <Image className={'absolute w-full h-full object-cover'} src={bgImage} alt={'Background'} />
+        <div className={'relative w-full h-full bg-black/40 py-32'}>
+          <h1 className={'text-5xl font-bold text-center'}>{title}</h1>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
